13a5f3593001628a43421164a31708a86a11ccfe
Ievhen <grabowski.ievhen@gmail.com>
2025-02-17 23:24:39 +0100

    Add basic support for loading configuration from .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+from .config import Config
 from .routes import route_blueprint
 
 
-def create_app():
+def create_app(config_cls=Config):
     app = Flask(__name__)
+    app.config.from_object(config_cls)
 
     configure_blueprints(app)
 
